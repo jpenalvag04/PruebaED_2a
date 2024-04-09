72c67fe4b87349ec9f1d9f7aa05e9701a07a8526
jpena <jpena@Javier.home>
2024-04-09 19:37:25 +0200

    Implementación del test TestConstructorPersona

---
+++ src/test/java/TestConstructorPersona.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit4TestClass.java to edit this template
+ */
+
+import instituto.Persona;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ *
+ * @author jpena
+ */
+public class TestConstructorPersona {
+    
+            @Test
+    public void testConstructor() {
+        Persona persona = new Persona(12345678, "Juan Perez", 'M', 1, 1, 2000);
+        
+        // Verificar que los valores se asignan correctamente en el constructor
+        assertEquals("Juan Perez", persona.getNombre());
+        assertEquals('M', persona.getGenero());
+        assertEquals(1, persona.getNacimiento().getDayOfMonth());
+        assertEquals(1, persona.getNacimiento().getMonthValue());
+        assertEquals(2000, persona.getNacimiento().getYear());
+    }
+}
