7f460c7933c39218f6d29df968c498988501eb53
jpena <jpena@Javier.home>
2024-04-09 19:38:24 +0200

    Implementación del test TestAñadirAlumnoNulo

---
+++ src/test/java/TestAñadirAlumnoNulo.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit4TestClass.java to edit this template
+ */
+
+import instituto.Curso;
+import instituto.Persona;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.fail;
+
+/**
+ *
+ * @author jpena
+ */
+public class TestAñadirAlumnoNulo {
+    
+        @Test
+    public void testAñadirAlumnoNulo() {
+        Curso curso = new Curso("Curso de Prueba");
+        Persona alumno = null;
+        
+        try {
+            curso.aniadirAlumno(alumno);
+            fail("Debería haber lanzado NullPointerException");
+        } catch (NullPointerException e) {
+            assertNotNull(e);
+        }
+    }
+}
